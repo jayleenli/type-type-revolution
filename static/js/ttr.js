5be3f6b97924a2c584fec1201347c66f2a99ff57
Jayleen Li <jayleenli@umail.ucsb.edu>
2019-02-02 01:11:24 -0800

    Added a WPM

--- static/js/ttr.js
+++ static/js/ttr.js
@@ -1,6 +1,9 @@
 //Put the whole text in the textAreaField into a giant array
 var words;
 populateWords();
+var started = 0;
+var timePassed = 0; //time passed in seconds
+var typedWords = 0.0;
 
 function populateWords()
 {
@@ -20,10 +23,41 @@ function populateWords()
 	document.getElementById("generated-text").innerHTML = innerHTMLString;
 }
 
+function startCountDown(duration) //duration in seconds
+{
+	var timer = duration;
 
+	var countdown = setInterval(function(){ 
+		//console.log(timer);
+		if (--timer < 0) {
+			//hit zero do something
+            clearInterval(countdown)
+        }
+	 }, 1000);
+}
+
+function passTime()
+{
+	setInterval(function(){ 
+		//console.log(timePassed);
+		timePassed = timePassed + .25; 
+		getWordPerMinute();
+	 }, 250);
+}
+
+function getWordPerMinute()
+{
+	document.getElementById("WPM").innerHTML = "WPM: " + Math.floor((typedWords/timePassed)*60) ;
+}
 //Check the typing text field for typing
 document.getElementById("user-text").addEventListener('keypress', function checkKeyPress(e) {
-
+	if (started === 0)
+	{
+		startCountDown(30);
+		passTime();
+		started = 1;
+	}
+	
 	if (e.keyCode == 32 || e.keyCode == 13) //space
 	{
 		//console.log(word_index);
@@ -37,6 +71,7 @@ document.getElementById("user-text").addEventListener('keypress', function check
 		{
 			//make the word red
 			document.getElementById(id).className = "highlight-green";
+			typedWords++;
 			//console.log("match");
 		}
 		else if (document.getElementById("user-text").value !== currentWord)
